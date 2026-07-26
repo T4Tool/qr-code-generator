c760f88c9b443c9fd4d0e9e360b7bd27b41463ca
BrahnaJyoji <brahnajyoji@gmail.com>
2026-07-26 18:29:55 +0530

    Setup GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/qr-code-generator/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
